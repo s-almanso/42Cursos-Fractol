73dfa616804c105011ce1bd71b7a72e01efd8325
s-almanso <salmanso@student.42abudhabi.ae>
2023-03-23 20:52:04 +0400

    first commit

---
+++ fractol/main.c
@@ -0,0 +1,14 @@
+#include "mlx.h"
+
+int main()
+{
+	void *mlx_ptr;
+	void *win_ptr;
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
+	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
+	mlx_loop(mlx_ptr);
+
+	return(0);
+}
