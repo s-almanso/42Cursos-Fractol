52b65033b415b38db9cc946e160d957611fe18d3
s-almanso <salmanso@student.42abudhabi.ae>
2023-03-25 15:01:51 +0400

    updated main
    create a window and init mlx

--- fractol/fractol.c
+++ fractol/fractol.c
@@ -6,21 +6,57 @@
 /*   By: salmanso <salmanso@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 20:19:07 by salmanso          #+#    #+#             */
-/*   Updated: 2023/03/24 20:19:08 by salmanso         ###   ########.fr       */
+/*   Updated: 2023/03/25 14:43:32 by salmanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "mlx.h"
+#include "fractol.h"
 
-int main()
+int select_fractol(char **argv, t_data *data)
 {
-	void *mlx_ptr;
-	void *win_ptr;
+	if(ft_strcmp(argv[1], "Mandelbrot") == 0)
+	{
+		data->fractol = 0;
+		mandelbrot_init(data);
+	}
+	else if	(ft_strcmp(argv[1], "Julia") == 0)
+	{
+		data->fractol = 0;
+		julia_init(data);
+	}
+	else if (ft_strcmp(argv[1], "Third fractol") == 0)
+	{
+		data->fractol = 0;
+		// init third fractol
+	}
+	else
+	{
+		ft_putendl("Error! Invalid input!");
+		return(0);
+	}
+	return (1);
+}
 
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "mlx 42");
-	mlx_pixel_put(mlx_ptr, win_ptr, 250, 250, 0xFFFFFF);
-	mlx_loop(mlx_ptr);
+int main(int argc, char **argv)
+{
+	t_data	*data;
 
-	return(0);
+	if (argc == 2)
+	{
+		data = (t_data*) malloc(sizeof(t_data));
+		data->mlx_ptr = mlx_init();
+		data->win_ptr = mlx_new_window(data->mlx_ptr, 900, 900, "Fractol 42");
+		if ((select_fractol(argv, data)) == 0)
+			return(-1);
+		mlx_key_hook(data->win_ptr, keys, data);
+		mlx_hook(data->win_ptr, 4, 0, mouse_hook, data);
+		mlx_loop(data->mlx_ptr);
+		free(data);
+	}
+	else if (argv > 2)
+		ft_putend("Error! Too many args!");
+	else
+		ft_putendl("Error! Please select desired Fractol!");
+	return (0);
 }
--- fractol/fractol.h
+++ fractol/fractol.h
@@ -5,8 +5,26 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: salmanso <salmanso@student.42abudhabi.ae>  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/03/24 20:19:13 by salmanso          #+#    #+#             */
-/*   Updated: 2023/03/24 20:19:14 by salmanso         ###   ########.fr       */
+/*   Created: 2023/03/25 14:46:34 by salmanso          #+#    #+#             */
+/*   Updated: 2023/03/25 14:46:46 by salmanso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+#include <unistd.h>
+#include <math.h>
+#include "mlx.h"
+
+typedef struct	s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+	int		fractol;
+	int		color;
+	int		width;
+	int		height;
+}	t_data;
+
+#endif
